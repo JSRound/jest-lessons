21aedeede829b6360729e72f444c8fee3c4c8e25
Jesus Redondo <github@jesusredondo.com>
2022-06-19 12:53:58 +0200

    Add test to match random property values of a collection

--- matchers/match-collection.test.js
+++ matchers/match-collection.test.js
@@ -1,17 +1,51 @@
-test('match collection', () => {
-  const state = [
-    { type: 'START', data: 'foo' },
-    { type: 'START', data: 'baz' },
-    { type: 'END', data: 'foo' }
-  ];
-  expect(state).toEqual(
-    // 1
-    expect.arrayContaining([
-      // 2
-      expect.objectContaining({
-        // 3
-        type: 'END' // 4
-      })
-    ])
-  );
+const { getProducts } = require('./db');
+
+test('match of a property for SOME element of the collection', () => {
+	const state = [
+		{ type: 'START', data: 'foo' },
+		{ type: 'START', data: 'baz' },
+		{ type: 'END', data: 'foo' }
+	];
+	expect(state).toEqual(
+		// 1
+		expect.arrayContaining([
+			// 2
+			expect.objectContaining({
+				// 3
+				type: 'END' // 4
+			})
+		])
+	);
+});
+
+test('failing match of a property for SOME element of the collection', () => {
+	const state = [
+		{ type: 'START', data: 'foo' },
+		{ type: 'START', data: 'baz' },
+		{ type: 'START', data: 'foo' }
+	];
+	expect(state).not.toEqual(
+		// 1
+		expect.arrayContaining([
+			// 2
+			expect.objectContaining({
+				// 3
+				type: 'END' // 4
+			})
+		])
+	);
+});
+
+test('match type of several properties for all the elements in a collection', async () => {
+	await expect(getProducts()).resolves.toEqual(
+		expect.arrayContaining([
+			expect.objectContaining({
+				id: expect.any(Number),
+				make: expect.any(String),
+				status: expect.any(Boolean),
+				createdat: expect.any(Date),
+				updatedat: expect.any(Date) // accepts Invalid Date
+			})
+		])
+	);
 });
